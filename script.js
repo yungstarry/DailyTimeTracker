f84ea360616ed01bb46d7679fc4a21450363e7a3
yungstarry <111007711+yungstarry@users.noreply.github.com>
2023-05-17 11:12:03 +0100

    to list added

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ window.addEventListener("load", function () {
         minutes: totalMinutes,
         timestamp: timestamp,
       };
-animateFlashcard()
+      animateFlashcard();
       saveTotalTime(updatedTotalTime);
       updateTimeDisplay(updatedTotalTime);
     }
@@ -90,3 +90,160 @@ addButton.addEventListener("click", () => {
   addTime();
   animateFlashcard();
 });
+
+
+// 
+
+
+
+
+// To-do-list
+// 
+// 
+// 
+
+// 
+const to_do_input = document.querySelector("#todo");
+const list = document.querySelector("#list");
+const to_do_form = document.querySelector("#to_do_form");
+const sc_feedback = document.querySelector("#sc_feedback");
+const heading = document.querySelector("#heading");
+
+// Load tasks from local storage
+window.addEventListener("load", function () {
+  const savedTasks = getSavedTasks();
+  savedTasks.forEach(function (task) {
+    addTaskToDOM(task);
+  });
+});
+
+// Execute functions on submit of the form
+to_do_form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let task = to_do_input.value.trim();
+  if (task !== "") {
+    addTaskToDOM(task);
+    saveTask(task);
+    removeValue(to_do_input);
+    screenReaderFeedback(task);
+  }
+});
+
+// We are using Event Delegation to listen for clicks on the list
+list.addEventListener("click", function (event) {
+  if (event.target.classList.contains("delete_task")) {
+    const deleteIcon = event.target;
+    const taskItem = deleteIcon.parentNode;
+    const taskName = taskItem.querySelector("label").textContent;
+    deleteTask(taskItem);
+    moveFocus(heading);
+    screenReaderFeedback(taskName, "Removed");
+    removeTaskFromStorage(taskName);
+  } else if (
+    event.target.tagName === "INPUT" &&
+    event.target.type === "checkbox"
+  ) {
+    const checkbox = event.target;
+    const taskItem = checkbox.parentNode;
+    const label = taskItem.querySelector("label");
+    if (checkbox.checked) {
+      label.style.textDecoration = "line-through";
+    } else {
+      label.style.textDecoration = "none";
+    }
+  }
+});
+
+function deleteTask(taskItem) {
+  list.removeChild(taskItem);
+}
+
+// We generate a new <li>, <input type="checkbox">, and <span>
+// each time a task is added to the DOM.
+function addTaskToDOM(task) {
+  const newID = generateID();
+  const taskItem = createElement("li", ["class", "task"]);
+  const theCheckbox = createElement("input", ["type", "checkbox"]);
+  const label = createElement("label");
+  label.textContent = task;
+  theCheckbox.setAttribute("id", newID);
+  taskItem.appendChild(theCheckbox);
+  taskItem.appendChild(label);
+  taskItem.appendChild(createDeleteIcon());
+  list.appendChild(taskItem);
+
+  // Add animation class to the newly added taskItem
+  taskItem.classList.add("animate");
+
+  // Remove animation class after animation duration
+  setTimeout(() => {
+    taskItem.classList.remove("animate");
+  }, 500);
+}
+
+function createElement(tagName, attribute = null) {
+  const node = document.createElement(tagName);
+  if (attribute) {
+    const [attr, value] = attribute;
+    node.setAttribute(attr, value);
+  }
+  return node;
+}
+
+function createDeleteIcon() {
+  const deleteIcon = createElement("i", [
+    "class",
+    "fas fa-trash-alt delete_task",
+  ]);
+  deleteIcon.style.color = "red"; // Set the icon color to red
+  deleteIcon.style.backgroundColor = "transparent"; // Set the icon background color to transparent
+  deleteIcon.style.border = "none"; // Remove icon border
+
+  return deleteIcon;
+}
+
+function removeValue(input) {
+  input.value = "";
+}
+
+// Sighted users are able to see when a task is added, but screen reader
+// users are more likely to be blind, so we want to make the
+// screen reader voice the actions.
+// We use the #sc_feedback element in the DOM to populate
+// the text which would be voiced
+function screenReaderFeedback(task, feedback = "Added") {
+  const feedbackElement = document.createElement("span");
+  feedbackElement.textContent = `${task} ${feedback}.`;
+  feedbackElement.style.color = feedback === "Added" ? "green" : "red";
+  sc_feedback.appendChild(feedbackElement);
+  setTimeout(() => {
+    sc_feedback.removeChild(feedbackElement);
+  }, 3000);
+}
+
+function generateID() {
+  const idPrefix = "task_num_";
+  const tasks = document.querySelectorAll("#list > li");
+  return idPrefix + tasks.length;
+}
+
+function moveFocus(element) {
+  element.focus();
+}
+
+function saveTask(task) {
+  const savedTasks = getSavedTasks();
+  savedTasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(savedTasks));
+}
+
+function getSavedTasks() {
+  const savedTasks = localStorage.getItem("tasks");
+  return savedTasks ? JSON.parse(savedTasks) : [];
+}
+
+function removeTaskFromStorage(task) {
+  const savedTasks = getSavedTasks();
+  const updatedTasks = savedTasks.filter((savedTask) => savedTask !== task);
+  localStorage.setItem("tasks", JSON.stringify(updatedTasks));
+}
